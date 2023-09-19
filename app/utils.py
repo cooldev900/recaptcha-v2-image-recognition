8299ab20e66fd0ddd532c51a54bdcc65be9d3429
cooldev900 <cooldev900@gmail.com>
2023-09-19 14:06:47 -0500

    update functions using colmun names

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,24 @@
 from PIL import Image
 import base64
 from loguru import logger
-from app.settings import CAPTCHA_RESIZED_IMAGE_FILE_PATH, CAPTCHA_TARGET_NAME_QUESTION_ID_MAPPING, MESSAGE_TEMPLATE, REPLACE_POSITIONS, PHONE_NUMBER_POSITION, MESSAGE_HISTORY_URL,FIRST_NAME_POSITION,LAST_NAME_POSITION, COMPANY_NAME_POSITION, TITLE_POSITION, EMAIL_ADDRESS_POSITION, STREET_ADDRESS_POSITION,CITY_POSITION,STATE_POSITION,ZIP_CODE_POSITION,COUNTRY_POSITION
+from app.settings import CAPTCHA_RESIZED_IMAGE_FILE_PATH, CAPTCHA_TARGET_NAME_QUESTION_ID_MAPPING, MESSAGE_TEMPLATE, PHONE_NUMBER, MESSAGE_HISTORY_URL, FIRST_NAME, LAST_NAME, COMPANY_NAME, TITLE, EMAIL_ADDRESS, STREET_ADDRESS, CITY, STATE, ZIP_CODE, COUNTRY
 import csv
+import os
 from datetime import datetime
+
+PROGRESS_FILE = "csv/progress.txt"
+
+
+def get_last_processed():
+    if os.path.exists(PROGRESS_FILE):
+        with open(PROGRESS_FILE, "r") as file:
+            return int(file.read().strip())
+    return 0
+
+
+def save_last_processed(row_num):
+    with open(PROGRESS_FILE, "w") as file:
+        file.write(str(row_num))
 
 
 def resize_base64_image(filename, size):
@@ -23,59 +38,58 @@ def get_question_id_by_target_name(target_name):
     logger.debug(f'question_id {question_id}')
     return question_id
 
+
 def convert_string_into_int(value):
-    if len(value) == 0: return -1
+    if len(value) == 0:
+        return -1
     try:
         return int(value)
     except ValueError:
         return -1
 
-# if column is set, return value. if not, return -1
-def get_position_infomation():
-    return {
-        'first_name': convert_string_into_int(FIRST_NAME_POSITION),
-        'last_name': convert_string_into_int(LAST_NAME_POSITION),
-        'company': convert_string_into_int(COMPANY_NAME_POSITION),
-        'title': convert_string_into_int(TITLE_POSITION),
-        'email': convert_string_into_int(EMAIL_ADDRESS_POSITION),
-        'street': convert_string_into_int(STREET_ADDRESS_POSITION),
-        'city': convert_string_into_int(CITY_POSITION),
-        'state': convert_string_into_int(STATE_POSITION),
-        'zip_code': convert_string_into_int(ZIP_CODE_POSITION),
-        'phone_number': convert_string_into_int(COUNTRY_POSITION),
-        'country': convert_string_into_int(COUNTRY_POSITION)
-    }
 
 def read_contacts_data(file_path):
-    position_info = get_position_infomation()
     contact_list = []
+    last_processed = get_last_processed()
     with open(file_path, 'r') as file:
-        csv_reader = csv.reader(file)
-        index = -1
-        for row in csv_reader:
-            index += 1
-            if index == 0: continue
+        csv_reader = csv.DictReader(file)
+        for index, row in enumerate(csv_reader, start=1):
+            # Resume from where we left off
+            if index <= last_processed:
+                continue
             contact_list.append({
                 'message': replace_values_into_templage(row),
-                'phone_number': '1' + row[PHONE_NUMBER_POSITION],
-                'first_name': row[position_info['first_name']] if position_info['first_name'] != -1 else "unknown",
-                'last_name': row[position_info['last_name']] if position_info['last_name']  != -1 else "unknown",
-                'company': row[position_info['company']] if position_info['company'] != -1 else "",
-                'title': row[position_info['title']] if position_info['title'] != -1 else "",
-                'email': row[position_info['email']] if position_info['email'] != -1 else "",
-                'street': row[position_info['street']] if position_info['street'] != -1 else "",
-                'city': row[position_info['city']] if position_info['city'] != -1 else "",
-                'state': row[position_info['state']] if position_info['state'] != -1 else "",
-                'zip_code': row[position_info['zip_code']] if position_info['zip_code'] != -1 else "",
-                'country': row[position_info['country']] if position_info['country'] != -1 else "",
+                'phone_number': '1' + row.get(PHONE_NUMBER, "").strip(),
+                'first_name': row.get(FIRST_NAME, "").strip() if len(FIRST_NAME) else "unknown",
+                'last_name': row.get(LAST_NAME, "").strip() if len(LAST_NAME) else "unknown",
+                'company': row.get(COMPANY_NAME, "").strip() if len(COMPANY_NAME) else "unknown",
+                'title': row.get(TITLE, "").strip() if len(TITLE) else "unknown",
+                'email': row.get(EMAIL_ADDRESS, "").strip() if len(FIRST_NAME) else "unknown",
+                'street': row.get(STREET_ADDRESS, "").strip() if len(STREET_ADDRESS) else "unknown",
+                'city': row.get(CITY, "").strip() if len(CITY) else "unknown",
+                'state': row.get(STATE, "").strip() if len(STATE) else "unknown",
+                'zip_code': row.get(ZIP_CODE, "").strip() if len(ZIP_CODE) else "unknown",
+                'country': row.get(COUNTRY, "").strip() if len(COUNTRY) else "unknown",
             })
+
+            # Save the progress
+            save_last_processed(index)
+
+        # If processing finishes, we can delete the progress file
+        if os.path.exists(PROGRESS_FILE):
+            os.remove(PROGRESS_FILE)
+
     return contact_list
+
 
 def replace_values_into_templage(row):
     message = MESSAGE_TEMPLATE
-    for value in REPLACE_POSITIONS.split(','):
-        message = message.replace(f'${value}', row[int(value)])
+    columns = [PHONE_NUMBER, MESSAGE_HISTORY_URL, FIRST_NAME, LAST_NAME, COMPANY_NAME,
+               TITLE, EMAIL_ADDRESS, STREET_ADDRESS, CITY, STATE, ZIP_CODE, COUNTRY]
+    for value in columns:
+        if len(value): message = message.replace(value, row.get(value, "").strip())
     return message
+
 
 def write_message_history(phone_number, message):
     current_date = datetime.now().strftime("%Y-%m-%d")
@@ -84,12 +98,14 @@ def write_message_history(phone_number, message):
         file.write(f'{current_time} phone number: {phone_number}\n')
         file.write(f'\t message: {message}\n')
 
+
 def contact_create_history(item):
     current_date = datetime.now().strftime("%Y-%m-%d")
     with open(f'{MESSAGE_HISTORY_URL}{current_date}.txt', 'a') as file:
         current_time = datetime.now().strftime("%H:%M:%S")
         file.write(f'{current_time} contact created\n')
         file.write(f'\t  Phone number: {item["phone_number"]} First name: {item["first_name"]} Last name: {item["last_name"]} Company: {item["company"]} Title: {item["title"]} Email Address: {item["email"]} Street Address: {item["street"]} City: {item["city"]} State: {item["state"]} Zip code: {item["zip_code"]} Country: {item["country"]} \n')
+
 
 def contact_create_failed_history(item):
     current_date = datetime.now().strftime("%Y-%m-%d")
